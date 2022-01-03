dbd8cd6e6eaeacdba52bf23348a61524e4fb7dbe
Waylon <919647870@qq.com>
2022-01-04 03:41:30 +0800

    first commit

---
+++ src/utils/sql/inception.js
@@ -0,0 +1,13 @@
+
+export function getSqlContent (sqlContent) {
+    const sqlContentList = sqlContent.split(';')
+    sqlContent = []
+    sqlContentList.map( (item) => {
+      if (item.length > 2) {
+        sqlContent.push({
+          value:item + ';',
+        })
+      }
+    })
+    return sqlContent
+  }
